e652caade72595ef0420223097e4c0e719c074cd
aditya <adityasu12@gmail.com>
2021-01-15 11:19:44 +0530

    fixed canvcas draw issue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canvas');
 const clear = document.querySelector("#clear");
+const div = document.getElementById('canvas-div');
 
 // Context for the canvas for 2 dimensional operations 
 const ctx = canvas.getContext('2d'); 
@@ -8,19 +9,19 @@ let coord = {x:0 , y:0};
  
 let paint = false;
 
-//resize();
-//window.addEventListener('resize',resize);
+resize();
+window.addEventListener('resize',resize);
+div.addEventListener('resize',resize);
 
 //set events
 canvas.addEventListener('mousedown',startPainting);
-canvas.addEventListener('onmouseup',stopPainting);
+canvas.addEventListener('mouseup',stopPainting);
 canvas.addEventListener('mousemove',sketch);
     
 function resize(){ 
-  canvas.width = window.innerWidth; 
-  canvas.height = window.innerHeight;
-  console.log(canvas.classList);
-  console.log("window= "+window.innerHeight,window.innerWidth); 
+  canvas.width = div.clientWidth; 
+  canvas.height = div.clientHeight;
+  console.log("div= "+ div.clientHeight, div.clientWidth); 
 } 
     
 function getPosition(event){ 
@@ -33,34 +34,25 @@ function getPosition(event){
 function startPainting(event){ 
   paint = true; 
   getPosition(event);
-  console.log(coord.x,coord.y);
+  console.log("start="+ coord.x,coord.y);
   //console.log(event.x,event.y);
 } 
 
 function stopPainting(){ 
   paint = false;
-  console.log("stop"); 
+  console.log("stop");
+  console.log("end= "+ coord.x,coord.y);
+  ctx.beginPath(); 
 } 
-
-function moving(event){
-  console.log("moving");
-}
     
 function sketch(event){ 
   //if (!paint) return; 
   if(event.buttons != 1) return;
 
-  //console.log("yes");
-  ctx.beginPath(); 
-    
   ctx.lineWidth = 5; 
   ctx.lineCap = 'round'; 
   ctx.strokeStyle = 'black'; 
       
-  // The cursor to start drawing 
-  // moves to this coordinate 
-  ctx.moveTo(coord.x, coord.y); 
-   
   // The position of the cursor 
   // gets updated as we move the 
   // mouse around. 
@@ -69,7 +61,6 @@ function sketch(event){
   // A line is traced from start 
   // coordinate to this coordinate 
   ctx.lineTo(coord.x , coord.y);
-  //  console.log(coord); 
     
   // Draws the line.
   ctx.stroke(); 
